b67ca2f4a857daef229179f4a08ec1feee79f728
Benjamin <cristea2003@gmail.com>
2024-10-31 16:33:35 +0100

    Obtenir dades List

--- src/org/milaifontanals/club/IClubOracleBD.java
+++ src/org/milaifontanals/club/IClubOracleBD.java
@@ -4,6 +4,8 @@
  */
 package org.milaifontanals.club;
 
+import java.util.List;
+
 /**
  *
  * @author beni
@@ -91,7 +93,7 @@ public interface IClubOracleBD {
      */
     void esborrarEquip(Equip e) throws GestorBDClub;
     
-    /**
+    /** 
      * Metode per modificar les dades d'un Equip de la BD
      * 
      * @param e Equip a modificar
@@ -99,6 +101,78 @@ public interface IClubOracleBD {
      */
     void modificarEquip(Equip e) throws GestorBDClub;    
     
+    /**
+     * Metode per afegir un Jugador a un Equip com a Membre a la BD
+     * 
+     * @param m Membre a afegir a la BD
+     * @throws org.milaifontanals.club.GestorBDClub
+     */
+    void afegirMembre(Membre m) throws GestorBDClub;
     
+     /**
+     * Metode per esborrar un Jugador d'un Equip de la BD
+     * 
+     * @param j Jugador a esborrar del equip
+     * @throws org.milaifontanals.club.GestorBDClub
+     */
+    void esborrarMembre(Jugador j) throws GestorBDClub;
+    
+    /**
+     * Metode per afegir un Usuari a la BD
+     * 
+     * @param u Usuari a afegir a la BD
+     * @throws org.milaifontanals.club.GestorBDClub
+     */
+    void afegirUsuari(Usuari u) throws GestorBDClub;
+    
+     /**
+     * Metode per esborrar un Usuari de la BD
+     * 
+     * @param u Usuari a esborrar de la BD
+     * @throws org.milaifontanals.club.GestorBDClub
+     */
+    void esborrarUsuari(Usuari u) throws GestorBDClub;
+    
+    /** 
+     * Metode per modificar les dades d'un Usuari de la BD
+     * 
+     * @param u Usuari a modificar
+     * @throws org.milaifontanals.club.GestorBDClub
+     */
+    void modificarUsuari(Usuari u) throws GestorBDClub;
+    
+     /**
+     * Obtenir tots els jugadors de la BD
+     *
+     * @return Llista de jugadors recuperats
+     * @throws org.milaifontanals.club.GestorBDClub
+     */
+    List<Jugador> obtenirLlistaJugador() throws GestorBDClub;
+    
+    /**
+     * Obtenir totes les Temporades de la BD
+     *
+     * @return Llista de Temporades recuperats
+     * @throws org.milaifontanals.club.GestorBDClub
+     */
+    List<Temporada> obtenirLlistaTemporada() throws GestorBDClub;
+    
+    /**
+     * Obtenir totes les Categoria de la BD
+     *
+     * @return Llista de Categoria recuperats
+     * @throws org.milaifontanals.club.GestorBDClub
+     */
+    List<Categoria> obtenirLlistaCategoria() throws GestorBDClub;
+    
+    /**
+     * Obtenir tots els Membres d'un equip de la BD
+     *
+     * 
+     * @param e Equip per recuperar els membres
+     * @return Llista de Membres recuperats
+     * @throws org.milaifontanals.club.GestorBDClub
+     */
+    List<Membre> obtenirLlistaMembre(Equip e) throws GestorBDClub;
     
 }
